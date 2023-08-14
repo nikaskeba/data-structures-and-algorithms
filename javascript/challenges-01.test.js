d79981f97597dabeebed469d7a76ab647d2129b7
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-14 16:54:34 -0700

    Finished first 6 challenges

--- javascript/challenges-01.test.js
+++ javascript/challenges-01.test.js
@@ -90,11 +90,14 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
-  // Solution code here...
+    arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+   for(let i = 0; i < times; i++) {
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -116,9 +119,17 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
-};
+ let groceryList = [];
 
+
+ availableItems.forEach(item => {
+    if (item.available) {
+      groceryList.push(item.name);
+    }
+  });
+
+  return groceryList;
+};
 /* ------------------------------------------------------------------------------------------------
 STRETCH - CHALLENGE 7
 
